ea46663c4d4534f5f5a865cb3d259fae096afc52
Doug Labbe <dougjlabbe@gmail.com>
2025-10-13 00:01:51 -0700

    Initial Commit

---
+++ Kinly/KinlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinlyApp.swift
+//  Kinly
+//
+//  Created by Douglas Labbe on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct KinlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
